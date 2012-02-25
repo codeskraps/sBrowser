d4e3ec6185df77ef85a0771be409f3241bb52c69
Carles Sentis <carlesentis@gmail.com>
2012-02-25 15:15:44 +0000

    licenses update

--- src/com/codeskraps/sbrowser/ListItemAdapter.java
+++ src/com/codeskraps/sbrowser/ListItemAdapter.java
@@ -1,15 +1,15 @@
 /**
- * List Template
- * Copyright (C) Carles Sentis 2011 <carlesentis@gmail.com>
+ * sBrowser
+ * Copyright (C) Carles Sentis 2011 <codeskraps@gmail.com>
  *
- * List Template is free software: you can
+ * sBrowser is free software: you can
  * redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later
  * version.
  *  
- * List Template is distributed in the hope that it
+ * sBrowser is distributed in the hope that it
  * will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU
