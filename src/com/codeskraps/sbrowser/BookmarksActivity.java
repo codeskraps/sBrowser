611a198fa59e54bf47b4f380fff3239d2a2f5ba2
Carles Sentis <carles@091labs>
2012-04-22 01:57:42 +0100

    Bookmark Activity

--- src/com/codeskraps/sbrowser/BookmarksActivity.java
+++ src/com/codeskraps/sbrowser/BookmarksActivity.java
@@ -23,7 +23,8 @@
 package com.codeskraps.sbrowser;
 
 import android.app.Activity;
-import android.app.Dialog;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -33,12 +34,13 @@ import android.view.ContextMenu.ContextMenuInfo;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.Toast;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class BookmarksActivity extends Activity implements OnItemClickListener {
 	private static final String TAG = BookmarksActivity.class.getSimpleName();
@@ -149,26 +151,36 @@ public class BookmarksActivity extends Activity implements OnItemClickListener {
 
 		if (position == 0) {
 
-			final Dialog dialog = new Dialog(this);
-
-			dialog.setContentView(R.layout.bookmark_dialog);
-			dialog.setTitle(getResources().getString(R.string.dialog_title));
-
-			final EditText dtxtName = (EditText) dialog.findViewById(R.id.dtxttitle);
-			dtxtName.setText(sBrowserData.getBookmarkItem().getName());
-
-			final EditText dtxtURL = (EditText) dialog.findViewById(R.id.dtxturl);
-			dtxtURL.setText(sBrowserData.getBookmarkItem().getUrl());
-
-			Button btnok = (Button) dialog.findViewById(R.id.btnok);
-			btnok.setOnClickListener(new View.OnClickListener() {
-
-				public void onClick(View v) {
-					Toast.makeText(BookmarksActivity.this, "clicked",
-							Toast.LENGTH_SHORT);
-					Log.d(TAG, "ok clicked");
-
-					BookmarkItem newBookmark = new BookmarkItem(dtxtName.getText().toString(), dtxtURL.getText().toString());
+			TextView txtName = new TextView(this);
+		    txtName.setText(getResources().getString(R.string.dialog_name));
+		    LayoutParams txtLayoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		    txtName.setLayoutParams(txtLayoutParams);
+		    
+		    final EditText edtName = new EditText(this);
+		    LayoutParams edtLayoutParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+		    edtName.setLayoutParams(edtLayoutParams);
+		    
+		    TextView txtUrl = new TextView(this);
+		    txtUrl.setText(getResources().getString(R.string.dialog_location));
+		    txtName.setLayoutParams(txtLayoutParams);
+		    
+		    final EditText edtUrl = new EditText(this);
+		    edtUrl.setLayoutParams(edtLayoutParams);
+		    
+		    LinearLayout dialogLayout = new LinearLayout(this);
+		    dialogLayout.setOrientation(LinearLayout.VERTICAL);
+		    dialogLayout.addView(txtName);
+		    dialogLayout.addView(edtName);
+		    dialogLayout.addView(txtUrl);
+		    dialogLayout.addView(edtUrl);
+			
+			final AlertDialog.Builder alertSearch = new AlertDialog.Builder(this);
+			alertSearch.setTitle(getResources().getString(R.string.dialog_title));
+			alertSearch.setView(dialogLayout);
+			alertSearch.setPositiveButton("Ok",	new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int whichButton) {
+							
+					BookmarkItem newBookmark = new BookmarkItem(edtName.getText().toString(), edtUrl.getText().toString());
 					dataBaseData.insert(newBookmark);
 					Log.d(TAG, "saved: "
 							+ newBookmark.getName());
@@ -178,21 +190,16 @@ public class BookmarksActivity extends Activity implements OnItemClickListener {
 					BookmarksActivity.this.finish();
 					
 					dialog.dismiss();
-				}
-			});
 
-			Button btnCancel = (Button) dialog.findViewById(R.id.btncancel);
-			btnCancel.setOnClickListener(new View.OnClickListener() {
+						}
+					});
 
-				public void onClick(View v) {
-					Toast.makeText(BookmarksActivity.this, "clicked",
-							Toast.LENGTH_SHORT);
-					Log.d(TAG, "Cancel clicked");
-					dialog.dismiss();
-				}
-			});
-
-			dialog.show();
+			alertSearch.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int whichButton) {
+							dialog.cancel();
+						}
+					});
+			alertSearch.show();
 
 		} else {
 			sBrowserData.setSelected(true);
