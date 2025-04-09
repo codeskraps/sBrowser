513aa4c8ccdd00a512a3a8c39adfeb2b18bc41b9
codeskraps <me@codeskraps.com>
2025-04-09 17:36:03 +0200

    Update version code and name in build.gradle.kts to reflect new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.codeskraps.sbrowser_new"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "3.3"
+        versionCode = 7
+        versionName = "4.0"
         setProperty("archivesBaseName", "sBrowser-v$versionName.$versionCode")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
